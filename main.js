71ab6128299552e0e2a6fe8120641c8c3620958e
Marek Sušický <susicky@rtsoft.cz>
2021-10-20 16:15:35 +0200

    !1 Hotfix for Kiko-chan

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 const Discord = require('discord.js');
+const play = require('./play');
 
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 
-const prefix = '!!!';
+const prefix = 'ě';
 
 client.once('ready', () => {
     console.log('Bot Online');
 })
 
-client.login('BOTKEY');
+client.login('login');
 
 client.on('message', message => {
     if(!message.content.startsWith(prefix) || message.author.bot) return;
@@ -18,7 +19,7 @@ client.on('message', message => {
     if (command === 'clear') {
         client.commands.get('clear').execute(message, args);
     } else if (command === 'play') {
-        client.commands.get('play').execute(message, args);
+        play.execute(message, args).then(r => console.log('Finished promise!'));
     } else if (command === 'leave') {
         client.commands.get('leave').execute(message, args);
     } else if(command === 'ping'){
